c353960b0e4725b1f346e173a7b398ff106230a5
Pedro Miguel Santos <45883143+Pedro-Miguel-123@users.noreply.github.com>
2025-01-10 16:49:14 +0000

    [proj-file] Fixed issue with project file

---
+++ PomodoroTimerFix/PomodoroTimerFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerFixApp.swift
+//  PomodoroTimerFix
+//
+//  Created by Pedro Santos on 10/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
